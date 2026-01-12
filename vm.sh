e6d6ad2ff27d406f024881d593a03d16ac366d4b
ZenseiTech <mrrangerplayz@gmail.com>
2026-01-12 15:29:09 +0530

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -84,21 +84,22 @@ fi
 # =============================
 echo "[INFO] Starting VM with 32 vCores as AMD Ryzen 9 7900..."
 
-# Keep host features but force AMD vendor and specific model string
-CPU_EMULATION="host,vendor=AuthenticAMD,+topoext"
+GPU_PCI_ADDR="01:00.0" 
+CPU_EMULATION="host,vendor=AuthenticAMD,+topoext,model-id=AMD Ryzen 9 7900 12-Core Processor @ 5.80GHz,kvm=off,hv_vendor_id=null,-hypervisor"
 
 exec qemu-system-x86_64 \
     -enable-kvm \
     -m "$MEMORY" \
+    -machine q35,accel=kvm,kernel_irqchip=on \
     -smp "$CPUS",cores=16,threads=2,sockets=1 \
-    -cpu "$CPU_EMULATION,model-id=AMD Ryzen 9 7900 12-Core Processor @ 5.80GHz" \
+    -cpu "$CPU_EMULATION" \
+    -device vfio-pci,host="$GPU_PCI_ADDR",multifunction=on \
     -drive file="$IMG_FILE",format=qcow2,if=virtio \
     -drive file="$SEED_FILE",format=raw,if=virtio \
     -boot order=c \
     -device virtio-net-pci,netdev=n0 \
     -netdev user,id=n0,hostfwd=tcp::"$SSH_PORT"-:22 \
     -nographic -serial mon:stdio
-
 
 
     # DONT CHANGE ANYTHING HERE
